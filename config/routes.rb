46554483ffc7b70bd8ef5f5ccb9be5f264f1a9cc
calozf <calozf@gmail.com>
2017-03-26 19:53:44 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   resources :blogposts
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
-  # get 'static_pages/index'
 
   root 'static_pages#landing_page'
+
+  get 'static_pages/landing_page'
+
+  get '/about', to: 'static_pages#about'
+
+  get '/contact', to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

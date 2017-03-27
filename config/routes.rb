07abac3cb0ed46b3a9ea044e4d8f83c9bbc97038
calozf <calozf@gmail.com>
2017-03-27 23:04:18 +0200

    added contact form

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'static_pages#contact'
 
+  post 'static_pages/thank_you'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
